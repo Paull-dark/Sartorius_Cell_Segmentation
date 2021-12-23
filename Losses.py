98522081dd2c5fc485d74880e1772d7980e3cdd5
Paull-dark <krivchenya.paul@gmail.com>
2021-12-23 10:17:07 +0800

    final Unet

--- Losses.py
+++ Losses.py
@@ -3,7 +3,6 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 from torch.autograd import Variable
-import segmentation_models_pytorch as smp
 
 try:
     from itertools import ifilterfalse
@@ -314,49 +313,3 @@ class FocalLoss2d(nn.Module):
         targets = torch.clamp(targets, eps, 1. - eps)
         pt = (1 - targets) * (1 - outputs) + targets * outputs
         return (-(1. - pt) ** self.gamma * torch.log(pt)).mean()
-    
-class ComboLossV2(nn.Module):
-    def __init__(self, weights, per_image=False, channel_weights=[1, 0.5, 0.5], channel_losses=None):
-        super().__init__()
-        self.weights = weights
-        self.bce = smp.losses.SoftBCEWithLogitsLoss()
-        self.dice = smp.losses.DiceLoss(mode='binary',)
-        self.jaccard = smp.losses.JaccardLoss(mode='binary') 
-        self.lovasz = LovaszLoss(per_image=per_image)
-        self.lovasz_sigmoid = LovaszLossSigmoid(per_image=per_image)
-        self.focal = smp.losses.FocalLoss()
-        self.mapping = {'bce': self.bce,
-                        'dice': self.dice,
-                        'focal': self.focal,
-                        'jaccard': self.jaccard,
-                        'lovasz': self.lovasz,
-                        'lovasz_sigmoid': self.lovasz_sigmoid}
-        self.expect_sigmoid = {'dice', 'focal', 'jaccard', 'lovasz_sigmoid'}
-        self.per_channel = {'dice', 'jaccard', 'lovasz_sigmoid'}
-        self.values = {}
-        self.channel_weights = channel_weights
-        self.channel_losses = channel_losses
-
-    def forward(self, outputs, targets):
-        loss = 0
-        weights = self.weights
-        sigmoid_input = torch.sigmoid(outputs)
-        for k, v in weights.items():
-            if not v:
-                continue
-            val = 0
-            if k in self.per_channel:
-                channels = targets.size(1)
-                for c in range(channels):
-                    if not self.channel_losses or k in self.channel_losses[c]:
-                        val += self.channel_weights[c] * self.mapping[k](
-                            sigmoid_input[:, c, ...] if k in self.expect_sigmoid else outputs[:, c, ...],
-                            targets[:, c, ...])
-
-            else:
-                val = self.mapping[k](sigmoid_input if k in self.expect_sigmoid else outputs, targets)
-
-            self.values[k] = val
-            loss += self.weights[k] * val
-        return loss.clamp(min=1e-5)
-  
